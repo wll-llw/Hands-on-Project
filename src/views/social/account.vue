8a5ae453e2db74262a46f08a48c048aaf0f1bbf8
邬鹭玲 <wululing@jz-ins.com>
2023-11-10 17:49:30 +0800

    feat: 录制屏幕并分享功能demo

--- src/views/social/account.vue
+++ src/views/social/account.vue
@@ -1,12 +1,45 @@
 <template>
   <div class="account wrap">
-    <div>This is an account page</div>
+    <button @click="shareScreen">共享屏幕</button>
   </div>
 </template>
 
 <script>
 import './style.scss'
-export default {}
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    shareScreen: async () => {
+      // 只有地址为localhost://, https://, 文件访问file:// 这三种情况可以调起设备，也就是navigator.mediaDevices不为undefined
+      console.log(navigator.mediaDevices, 'navigator.mediaDevices')
+      const stream = await navigator.mediaDevices.getDisplayMedia({
+        video: true
+      })
+      const mime = MediaRecorder.isTypeSupported("video/webm;codecs=h264")
+          ? "video/webm;codecs=h264"
+          : "video/webm";
+
+      const mediaRecorder = new MediaRecorder(stream, { mimeType: mime });
+
+      const chunks = [];
+      mediaRecorder.addEventListener("dataavailable", function (e) {
+        chunks.push(e.data);
+      });
+
+      mediaRecorder.addEventListener("stop", () => {
+        const blob = new Blob(chunks, { type: chunks[0].type });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "video.webm";
+        a.click();
+      });
+      mediaRecorder.start();
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
